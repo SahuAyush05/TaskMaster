5c722d2586af205fef8a8883555282afa79a1f6b
Ayush Sahu <ayushsahu050502@gmail.com>
2025-05-21 23:30:34 +0530

    Boilerplate setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        Hii, himani ayush this side
+      </div>
+    </>
+  )
+}
+
+export default App
